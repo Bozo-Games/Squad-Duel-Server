a8e6eb821c2f80a535f1c197df4d8fec95f932a2
Bill Decker <bill.s.decker@gmail.com>
2017-12-09 14:31:19 -0800

    hold 2

--- debug.js
+++ debug.js
@@ -5,6 +5,9 @@ const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
+var logger = fs.createWriteStream('log.txt', {
+	flags: 'a' // 'a' means appending (old data will be preserved)
+});
 
 let classNames = Object.keys(cardDB.characterClasses);
 let titleNames = Object.keys(cardDB.characterTitles);
@@ -24,16 +27,16 @@ for(let ti = 0; ti < titleNames.length; ti++) {
 	for(let ci = 0; ci < classNames.length; ci++) {
 		let i = Math.floor(Math.random()*options.length);
 		let option = options[i];
-		console.log(`"${titleNames[ti]} ${classNames[ci]}": {`);
+		logger.write(`"${titleNames[ti]} ${classNames[ci]}": {`);
 
 		if (!fs.existsSync(`./client/Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}`)){
 			fs.mkdirSync(`./client/Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}`);
 		}
 		for(let li = 0; li < loops.length; li++) {
 			let loop = loops[li];
-			console.log(`\t${loop.name}: [`);
+			logger.write(`\t${loop.name}: [`);
 			for(let j = 0; j < loop.count;j++) {
-				console.log(`\t\t'./Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}/${loop.name}/${j}.png',`);
+				logger.write(`\t\t'./Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}/${loop.name}/${j}.png',`);
 				if (!fs.existsSync(`./client/Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}/${loop.name}`)){
 					fs.mkdirSync(`./client/Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}/${loop.name}`);
 				}
@@ -41,14 +44,13 @@ for(let ti = 0; ti < titleNames.length; ti++) {
 					`./temp/hero_${option.i}/${option.g}/${li+1}_${loop.name}/${j+1}.png`,
 					`./client/Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}/${loop.name}/${j}.png`,
 					function(err) {
-					if ( err ) console.log('ERROR: ' + err);
-				});
+						if ( err ) logger.write('ERROR: ' + err);
+					});
 			}
-			console.log(`\t]`);
+			logger.write(`\t],`);
 		}
-		setTimeout(function () {
-
-		},5000);
+		logger.write(`\tdead: './Assets/icons/card/character/${titleNames[ti]}_${classNames[ci]}/die/11.png',`);
 		options.splice(i,1);
 	}
 }
+logger.end();
