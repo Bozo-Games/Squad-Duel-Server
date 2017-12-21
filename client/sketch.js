1e5b539be20388eedd0f4f98966c154b1dd06123
Bill Decker <bill.s.decker@gmail.com>
2017-12-20 17:14:24 -0800

    play this now

--- client/sketch.js
+++ client/sketch.js
@@ -17,7 +17,25 @@ function windowResized() {
 function draw() {
 	frameTime = (new Date()).getTime();
 	background(180);
-	if(currentGame !== undefined) {	currentGame.draw(); }
+	if(currentGame !== undefined) {
+		if(currentGame.currentState === 'endGame') {
+			textAlign(CENTER,CENTER);
+			textStyle(30);
+			let aliveCount = 0;
+			for(let card in currentGame.playerHand.cards) {
+				if(card.currentState !== 'dead') {
+					aliveCount++;
+				}
+			}
+			if(aliveCount > 0) {
+				text("YOU WON",width/2,height/2);
+			} else {
+				text("YOU LOST",width/2,height/2);
+			}
+		} else {
+			currentGame.draw();
+		}
+	}
 }
 function touchEnded() {
 	if(currentGame !== undefined) {currentGame.touchEnded();}
